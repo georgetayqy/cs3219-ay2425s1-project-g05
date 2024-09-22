96f18c33b43c3d97a4383da3217964696a9924cb
Lim Pei En <111886863+peienlim@users.noreply.github.com>
2024-09-22 10:38:15 +0800

    chore: setup frontend with Vite, React, TypeScript, and Mantine

---
+++ peer-prep/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+
+  return <MantineProvider> {
+    <h1>Peer-Prep</h1>
+  }</MantineProvider>
+}
+
+export default App
