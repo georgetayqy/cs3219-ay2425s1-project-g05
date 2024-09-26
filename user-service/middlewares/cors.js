01b319449cdf1fdd0c6959448cb00fa069579e3a
pzl111 <phuazailian@gmail.com>
2024-09-26 16:15:56 +0800

    chore: Update port comments in cors.js

--- user-service/middlewares/cors.js
+++ user-service/middlewares/cors.js
@@ -1,10 +1,10 @@
 import cors from "cors"
 
 const allowedOrigins = [
-    "http://localhost:3000", // Frontend
-    "http://localhost:8000", // user service
-    "http://localhost:8001", // matching service
-    "http://localhost:8002", // question service
+    "http://localhost:8000", // frontend
+    "http://localhost:8001", // user service
+    "http://localhost:8002", // matching service
+    "http://localhost:8003", // matching service
     "http://localhost:8004", // collaboration service
 ];
 
