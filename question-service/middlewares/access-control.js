39c33cc90b6c87ff9c95e6ebaee2eb6d700ff65d
elaineshijie <lohelaineshijie.183@gmail.com>
2024-09-29 11:29:12 +0800

    chore: decrypt accessToken

--- question-service/middlewares/access-control.js
+++ question-service/middlewares/access-control.js
@@ -1,15 +1,35 @@
 import ForbiddenError from "../errors/ForbiddenError.js";
+import jwt from 'jsonwebtoken';
+
+function verifyAccessToken(token) {
+  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return null;
+    }
+    return user;
+  });
+}
 
 const checkAdmin = (req, res, next) => {
-    // Assuming 'isAdmin' is stored in cookies
-    console.log(req.cookies);
-    const { isAdmin }= req.cookies;
+  // TODO: Remove after isAdmin is stored in cookies 
+  console.log(req.cookies);
+  if (!req.cookies.accessToken) {
+    throw new ForbiddenError("Access Token not found");
+  }
+  const user = verifyAccessToken(req.cookies.accessToken);
+  if (!user || !user.isAdmin) {
+    throw new ForbiddenError("Access Token verification failed");
+  }
+  const isAdmin = user.isAdmin;
 
-    if (isAdmin) {
-        return next();
-    } else {
-        throw new ForbiddenError("You are not authorized to perform this action");
-    }
-}
+  // Assuming 'isAdmin' is stored in cookies
+  // const { isAdmin } = req.cookies;
+
+  if (isAdmin) {
+    return next();
+  } else {
+    throw new ForbiddenError("You are not authorized to perform this action");
+  }
+};
 
 export default checkAdmin;
