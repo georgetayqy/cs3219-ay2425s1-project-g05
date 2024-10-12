6f0b58bc7df9894b351a531a8a4940f459d34ba9
hollag <marcussoh38@gmail.com>
2024-10-12 21:28:58 +0800

    fix: losing navigation state when refreshing

--- peer-prep/src/pages/ProtectedRouteWrapper.tsx
+++ peer-prep/src/pages/ProtectedRouteWrapper.tsx
@@ -1,10 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import { AUTH_STATUS, useAuth } from "../hooks/useAuth";
 
 // https://stackoverflow.com/questions/70833727/using-react-router-v6-i-need-a-navbar-to-permanently-be-there-but-cant-display
 export default function ProtectedRouteWrapper() {
-  const { user } = useAuth();
-  if (!user) {
+  const { user, authStatus } = useAuth();
+
+  if (authStatus === AUTH_STATUS.LOADING) {
+    return <></>;
+  }
+
+  if (authStatus === AUTH_STATUS.LOGGED_OUT) {
+    console.log("not auth bro");
     // user is not authenticated
     return <Navigate to="/login" />;
   }
