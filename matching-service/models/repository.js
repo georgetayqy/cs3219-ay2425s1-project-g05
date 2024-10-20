b2a9475e5c776b4b7ea9e14e00a6fefdf57e9fe1
hollag <marcussoh38@gmail.com>
2024-10-20 14:53:42 +0800

    fix: forgot to merge

--- matching-service/models/repository.js
+++ matching-service/models/repository.js
@@ -1,13 +1,6 @@
 import PendingUserModel from "./pendingUser-model.js";
 
 export async function _findPendingUserByCriteria({ difficulties, categories, email }) {
-<<<<<<< HEAD
-    return PendingUserModel.findOne({
-        difficulties: { $in: difficulties },
-        categories: { $in: categories },
-        email: { $ne: email }
-    });
-=======
     return PendingUserModel
         .findOne({
             difficulties: { $in: difficulties },
@@ -15,7 +8,6 @@ export async function _findPendingUserByCriteria({ difficulties, categories, ema
             email: { $ne: email }
         })
         .sort({ priority: -1 });
->>>>>>> origin/matching-service
 }
 
 export async function _deletePendingUserByEmail(email) {
@@ -30,18 +22,14 @@ export async function _createPendingUser(param) {
     return PendingUserModel.create(param);
 }
 
-export async function _deletePendingUserById(socketId) {
+export async function _deletePendingUserBySocketId(socketId) {
     return PendingUserModel.findOneAndDelete({ socketId: socketId });
 }
 
 export async function _findAllPendingUsers() {
-<<<<<<< HEAD
-    return PendingUserModel.find({});
-=======
     return PendingUserModel.find({}).sort({ priority: -1 });
 }
 
 export async function _deletePendingUserByDocId(docId) {
     return PendingUserModel.findOneAndDelete({ _id: docId });
->>>>>>> origin/matching-service
 }
