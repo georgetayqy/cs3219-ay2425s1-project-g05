327afe216510a319d84c8170b0ac5f94d82f7bb3
George Tay <georgetay71@gmail.com>
2024-10-26 17:23:30 +0800

    chore: update CORS configuration

--- collaboration-service/src/middlewares/cors.js
+++ collaboration-service/src/middlewares/cors.js
@@ -4,6 +4,9 @@ const allowedOrigins = [
   'http://localhost:5173', // frontend dev
   'http://peerprep.s3-website-ap-southeast-1.amazonaws.com', // frontend prod
   'http://peerprep-frontend-bucket.s3-website-ap-southeast-1.amazonaws.com', // frontend staging
+  'https://csppwicfm9.execute-api.ap-southeast-1.amazonaws.com',
+  'http://a0bc731fbaa9f498984db9ee4e3d3ff5-1870679687.ap-southeast-1.elb.amazonaws.com',
+  'http://redis-001.mrdqdr.0001.apse1.cache.amazonaws.com:6379',
   'http://localhost:8001', // user service
   'http://localhost:8002', // matching service
   'http://localhost:8003', // question service
@@ -14,6 +17,8 @@ const corsOptions = {
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
+    'Content-Type',
+    'Authorization',
     'Content-Type',
     'Authorization',
     'X-Amz-Date',
