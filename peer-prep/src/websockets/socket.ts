6d622f8eb698fc521240aae37272221a51ae30f4
hollag <marcussoh38@gmail.com>
2024-10-31 00:24:55 +0800

    chore: update to align with userId instead

--- peer-prep/src/websockets/socket.ts
+++ peer-prep/src/websockets/socket.ts
@@ -5,4 +5,7 @@ const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhos
 
 export const socket = io(URL, {
   autoConnect: false,
+  path: "/api/matching-service",
+  transports: ["websocket"],
+  withCredentials: true,
 });
