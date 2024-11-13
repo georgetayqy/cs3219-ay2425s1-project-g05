16e1323a32e4d4de1e177ebafa05d5c5db40db17
hollag <marcussoh38@gmail.com>
2024-11-13 11:31:29 +0800

    fix: enable env

--- user-service/middlewares/redis.js
+++ user-service/middlewares/redis.js
@@ -1,8 +1,9 @@
 import { createClient } from 'redis';
-
+import dotenv from 'dotenv';
+dotenv.config();
 console.log("[user-service] connecting to redis with url " + process.env.REDIS_URL);
 const client = createClient({
-    url: "redis://localhost:6379" || 'redis://redis:6379' // Adjust the URL if using Docker or a remote instance
+    url: process.env.REDIS_URL || 'redis://redis:6379' // Adjust the URL if using Docker or a remote instance
 });
 
 client.on("ready", () => console.log("Connected to redis!"));
